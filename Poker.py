d2e4e498483e46aa55f0f16d987be2cadec43d31
Cowboyserg <skuzmenkov@list.ru>
2018-02-01 15:49:51 +0300

    version 0.2

--- Poker.py
+++ Poker.py
@@ -1,3 +1,4 @@
+import itertools
 carts=[]
 cd=[]
 cm=[]
@@ -9,98 +10,48 @@ for st in file:
     cm.append(m)
     carts.append([m,k])
 file.close()
-print(cd)
-print(carts)
+print("Массив достоинств: ", cd)
+print("Массив карт: ", carts)
 parax2 = 0
 parax = 0
 nomer=-1
 set3=0
+nomeraseta = []
 #Первый раз проходим, parax2 = 1, count = 2
 #Второй раз проходим, parax2 = 2, count = 2
 #Третий раз проходим, parax2 = 3, count = 2
 #Четвертый раз проходим, parax2 = 4, count = 2
 combo = {0:None,1:None,2:[None,None],3:None,4:None,5:None,6:[None,None],7:None,8:None,9:None}
-for i in cd:#проходим массив ck, i = достоинству карты, проходим его количество элементов раз
+#0:старшая карта 1:пара 2:две пары 3:сет 4:стрит 5:флеш 6:фулл хаус 7:каре 8:стрит флеш 9:флеш рояль
+for i in cd:#проходим массив cd, i = достоинству карты, проходим его количество элементов раз
      count=cd.count(i)# количество повторений данного достоинства
      if count == 2:#пара
          combo[1] = i
-         if count == 3:#сет
-             combo[3] = i
+     elif count == 3:#сет
+         combo[3] = i
+         for j in range(7):
+             if cd[j] == combo[3]:
+                 nomeraseta.append(j)
 
      if count == 4:
          combo[2] = [combo[1], i]
+
+print("Сет под номерами :", nomeraseta)
 #Флеш
 for i in cm:
     count = cm.count(i)
     if count == 5:
         combo[5] = i
 #стрит
-j = 0
-for i in range(carts):
-    if cd[0] == j:
-        if cd[1] == j+1:
-            if cd[2] == j+2:
-                if cd[3] == j+3:
-                    if cd[4] == j+4:
-                        if cd[5] == j+5:
-                            print("Стрит")
-                        if cd[6] == j+6:
-                            print("Стрит")
-                    if cd[5] == j+4:
-                        if cd[4] == j+5:
-                            print("Стрит")
-                        if cd[6] == j+6:
-                            print("Стрит")
-                    if cd(6) == j+4:
-                        if cd[4] == j+5:
-                            print("Стрит")
-                        if cd[6] == j+6:
-                            print("Стрит")
+for p in itertools.permutations(cd):
+    for j in range(6):
+        if ((p[0] == j) and (p[1] == j+1) and (p[2] == j+2) and (p[3] == j+3) and (p[4] == j+4)):
+            #print(p)
+            combo[4] = 1
+#фулл хаус
 
 print(combo)
 exit(0)
-
-
-
-'''carts=[]
-cd=[]
-cm=[]
-file=open("input.txt", mode="r")
-for st in file:
-    m,k=st.split(",")
-    m,k=int(m),int(k)
-    cd.append(k)
-    cm.append(m)
-    carts.append([m,k])
-file.close()
-print(cd)
-print(carts)
-combo=[]
-#определение пары
-nomer=-1
-for i in cd:#проходим массив ck, i = элементу массива, проходим его количество элементов раз
-     count=cd.count(i)#
-     nomer += 1
-     if count == 1:
-         combo.append(i)
-         continue
-     if count >= 2:
-         combo.append(i)
-         print("Обнаружена пара для карты ", carts[nomer])
-         if count >= 3:
-             print("Обнаружен сет")
-
-print(combo)
-exit(0)'''
-
-
-
-
-
-
-
-
-
 
 '''
 carts=[]
@@ -150,4 +101,79 @@ if (set3 == 3) and (parax2 == 2):
 print(combo)
 exit(0)
 
+'''
+
+
+
+
+
+'''
+for i in range(7):
+    if cd[0] == j:
+        if cd[1] == j+1:
+            if cd[2] == j+2:
+                if cd[3] == j+3:
+                    if cd[4] == j+4:
+                        if cd[5] == j+5:
+                            print("Стрит")
+                        if cd[6] == j+6:
+                            print("Стрит")
+                    if cd[5] == j+4:
+                        if cd[4] == j+5:
+                            print("Стрит")
+                        if cd[6] == j+6:
+                            print("Стрит")
+                    if cd[6] == j+4:
+                        if cd[4] == j+5:
+                            print("Стрит")
+                        if cd[5] == j+6:
+                            print("Стрит")
+                if cd[4] == j+3:
+                    if cd[3] == j+4:
+                        if cd[5] == j+5:
+                            print("Стрит")
+                        if cd[6] == j+6:
+                            print("Стрит")
+                    if cd[5] == j+4:
+                        if cd[3] == j+5:
+                            print("Стрит")
+                        if cd[6] == j+6:
+                            print("Стрит")
+                    if cd[6] == j+4:
+                        if cd[3] == j+5:
+                            print("Стрит")
+                        if cd[5] == j+6:
+                            print("Стрит")
+                if cd[5] == j+3:
+                    if cd[3] == j+4:
+                        if cd[4] == j+5:
+                            print("Стрит")
+                        if cd[6] == j+6:
+                            print("Стрит")
+                    if cd[4] == j+4:
+                        if cd[3] == j+5:
+                            print("Стрит")
+                        if cd[6] == j+6:
+                            print("Стрит")
+                    if cd[6] == j+4:
+                        if cd[3] == j+5:
+                            print("Стрит")
+                        if cd[4] == j+6:
+                            print("Стрит")
+                if cd[6] == j+3:
+                    if cd[3] == j+4:
+                        if cd[4] == j+5:
+                            print("Стрит")
+                        if cd[5] == j+6:
+                            print("Стрит")
+                    if cd[4] == j+4:
+                        if cd[3] == j+5:
+                            print("Стрит")
+                        if cd[5] == j+6:
+                            print("Стрит")
+                    if cd[5] == j+4:
+                        if cd[3] == j+5:
+                            print("Стрит")
+                        if cd[4] == j+6:
+                            print("Стрит")
 '''
---
+++ perebor.py
@@ -0,0 +1,10 @@
+import itertools
+i=1
+mas = [None, None, None]
+a = 1
+b = 2
+c = 3
+while i <= 3:
+    mas.append(a)
+    i+=1
+print(mas)
